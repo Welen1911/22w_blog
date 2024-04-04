66ed6ac3bdf176af7a0b84518b074b1e8d476f96
Welen1911 <welenalmeida2210@gmail.com>
2024-04-03 21:00:10 -0300

    Primeiro commit

---
+++ resources/js/Components/Footer.vue
@@ -0,0 +1,12 @@
+<template>
+    <footer class="bg-gray-900 pb-10">
+        <p class="block text-center text-sm mb-6">
+            Built by
+            <a target="_blank" href="https://github.com/reeseschultz">
+                Reese Schultz
+            </a>
+        </p>
+
+        <a class="block text-center text-sm" href="https://reeseschultz.github.io/11r/privacy-policy">Privacy Policy</a>
+    </footer>
+</template>
